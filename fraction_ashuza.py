5c2b65d81171f1c0de39ce44afcbf6c01ca39f52
GloireBazishula <159158293+GloireBazishula@users.noreply.github.com>
2024-02-07 11:29:34 +0100

    je bosse sur le tp
    je bosse petit à petit muke muke

--- fraction_ashuza.py
+++ fraction_ashuza.py
@@ -5,3 +5,16 @@ Created on Wed Feb  7 10:36:58 2024
 @author: bazish
 """
 
+class Fraction:
+    
+    def __init__(self,_num, _den):
+        self._num = _num
+        
+        if _den > 0:
+            self._den = _den
+        else:
+            return "Denominateur doit etre > 0"
+        
+        
+
+        
